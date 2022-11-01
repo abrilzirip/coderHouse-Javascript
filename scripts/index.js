2ee4ec4d871a9779295770a12cf19398dd8ee296
Fernando Abril Piriz <fernandoapiriz@gmail.com>
2022-10-31 22:43:27 -0300

    Agregado de código en archivo Js

---
+++ scripts/index.js
@@ -0,0 +1,144 @@
+// Peliculas:
+let titulo1 = "Viaje a la Luna";
+let tituloOriginal1 = "Le voyage dans la lune";
+let anio1 = 1902;
+let duracion1 = 13; //Minutos
+let genero1 = "Corto, Acción, Aventura";
+let sinopsis1 = "Un grupo de astrónomos realiza una expedición a la Luna.";
+let puntaje1 = 8.2; 
+
+let titulo2 = "El nacimiento de una nación";
+let tituloOriginal2 = "The Birth of a Nation";
+let anio2 = 1915;
+let duracion2 = 195; //Minutos
+let genero2 = "Drama, Historia, Bélico";
+let sinopsis2 = "La familia Stoneman encuentra su amistad con los Cameron afectados por la Guerra Civil, ambos luchando en ejércitos opuestos. El desarrollo de la guerra en sus vidas juega hasta el asesinato de Lincoln y el nacimiento del Ku Klux Klan.";
+let puntaje2 = 6.2;
+
+let titulo3 = "El gabinete del Dr. Caligari";
+let tituloOriginal3 = "Das Cabinet des Dr. Caligari";
+let anio3 = 1920;
+let duracion3 = 50; //Minutos
+let genero3 = "Teror, Misterio, Suspense";
+let sinopsis3 = "El doctor Caligari, un hipnotista, usa a Cesare, sonámbulo, para cometer asesinatos.";
+let puntaje3 = 8;
+
+let titulo4 = "El moderno Sherlock Holmes";
+let tituloOriginal4 = "Sherlock Jr.";
+let anio4 = 1924;
+let duracion4 = 45; //Minutos
+let genero4 = "Acción, Comedia, Romance";
+let sinopsis4 = "Un proyeccionista anhela convertirse en detective y la oportunidad se le presenta cuando un competidor le tiende una trampa.";
+let puntaje4 = 8.2;
+
+let titulo5 = "Tabú";
+let tituloOriginal5 = "Tabu: A Story of the South Seas";
+let anio5 = 1931;
+let duracion5 = 86; //Minutos
+let genero5 = "Aventura, Drama, Romance";
+let sinopsis5 = "En la isla de Bora Bora, en el Pacífico Sur, el amor de una joven pareja se ve amenazado cuando el jefe tribal declara a la niña virgen sagrada.";
+let puntaje5 = 7.4;
+
+let titulo6 = "Pinocho";
+let tituloOriginal6 = "Pinocchio";
+let anio6 = 1940;
+let duracion6 = 86; //Minutos
+let genero6 = "Animación, Aventura, Comedia";
+let sinopsis6 = "Una marioneta cobra vida y, con un grillo como conciencia, debe demostrar que merece ser un niño de verdad.";
+let puntaje6 = 7.5;
+
+let titulo7 = "Casablanca";
+let tituloOriginal7 = "Casablanca";
+let anio7 = 1942;
+let duracion7 = 102; //Minutos
+let genero7 = "Drama, Romance, Bélico";
+let sinopsis7 = "Un cínico expatriado norteamericano, dueño de un café, se debate entre ayudar o no a su antigua amante y a su marido fugitivo a escapar de los nazis en el Marruecos francés.";
+let puntaje7 = 8.5;
+
+let titulo8 = "La naranja mecánica";
+let tituloOriginal8 = "A Clockwork Orange";
+let anio8 = 1971;
+let duracion8 = 136; //Minutos
+let genero8 = "Crimen, Ciencia ficción";
+let sinopsis8 = "En el futuro, el sádico líder de una banda es encarcelado y se ofrece como voluntario para un experimento de reeducación, pero no sale según lo planeado.";
+let puntaje8 = 8.3;
+
+let titulo9 = "El padrino";
+let tituloOriginal9 = "The Godfather";
+let anio9 = 1972;
+let duracion9 = 175; //Minutos
+let genero9 = "Crimen, Drama";
+let sinopsis9 = "El envejecido patriarca de una dinastía del crimen organizado en la ciudad de Nueva York de la posguerra transfiere el control de su imperio clandestino a su reacio hijo menor.";
+let puntaje9 = 9.2;
+
+let titulo10 = "Chinatown";
+let tituloOriginal10 = "Chinatown";
+let anio10 = 1974;
+let duracion10 = 130; //Minutos
+let genero10 = "Drama, Misterio, Suspenso";
+let sinopsis10 = "Un detective privado, contratado para desenmascarar a un adúltero en Los Ángeles de los años 30, se ve envuelto en una red de engaños, corrupción y asesinato.";
+let puntaje10 = 8.2;
+
+let titulo11 = "Saló o los 120 días de Sodoma";
+let tituloOriginal11 = "Salò o le 120 giornate di Sodoma";
+let anio11 = 1975;
+let duracion11 = 117; //Minutos
+let genero11 = "Drama";
+let sinopsis11 = "Durante la Segunda Guerra Mundial en Italia, cuatro fascistas libertinos acorralan a nueve adolescentes y los someten a ciento veinte días de tortura física, mental y sexual.";
+let puntaje11 = 5.8;
+
+let titulo12 = "E.T. El extraterrestre";
+let tituloOriginal12 = "";
+let anio12 = 1982;
+let duracion12 = 115; //Minutos
+let genero12 = "Aventura, Familiar, Ciencia ficción";
+let sinopsis12 = "Un niño con problemas se arma de valor para ayudar a un amistoso alienígena a escapar de la Tierra y regresar a su mundo natal.";
+let puntaje12 = 7.9;
+
+let titulo13 = "Regreso al futuro";
+let tituloOriginal13 = "Back to the Future";
+let anio13 = 1985;
+let duracion13 = 116; //Minutos
+let genero13 = "Aventura, Comedia, Ciancia ficción";
+let sinopsis13 = "Marty McFly, un estudiante de secundaria de 17 años, es enviado accidentalmente 30 años al pasado en un DeLorean que viaja en el tiempo inventado por su amigo cercano, el científico inconformista Doc Brown.";
+let puntaje13 = 8.5;
+
+
+let titulo14 = "Pretty Woman";
+let tituloOriginal14 = "Pretty Woman";
+let anio14 = 1990;
+let duracion14 = 119; //Minutos
+let genero14 = "Coemdia, Romance";
+let sinopsis14 = "Un hombre en un negocio legal pero dañino necesita una acompañante para asistir a eventos sociales, y contrata a una hermosa prostituta... de la que se enamora.";
+let puntaje14 = 7.1;
+
+let titulo15 = "Jurassic Park (Parque Jurásico)";
+let tituloOriginal15 = "Jurassic Park";
+let anio15 = 1193;
+let duracion15 = 127; //Minutos
+let genero15 = "Acción, Aventura, Ciencia ficción";
+let sinopsis15 = "Un paleontólogo pragmático que recorre un parque temático casi completo en una isla de América Central tiene la tarea de proteger a un par de niños después de que un corte de energía hace que los dinosaurios clonados del parque se suelten.";
+let puntaje15 = 8.2;
+
+let titulo16 = "Los padres de ella";
+let tituloOriginal16 = "Meet the Parents";
+let anio16 = 2000;
+let duracion16 = 108; //Minutos
+let genero16 = "Comedia, Romance";
+let sinopsis16 = "El enfermero Greg Focker conoce a los padres de su novia, a quien quiere pedirle que se case con él. El padre resulta ser la peor pesadilla de cualquier novio";
+let puntaje16 = 7;
+
+let titulo17 = "Secreto en la montaña";
+let tituloOriginal17 = "Brokeback Mountain";
+let anio17 = 2005;
+let duracion17 = 134; //Minutos
+let genero17 = "Drama, Romance";
+let sinopsis17 = "La historia de una relación prohibida y secreta entre dos vaqueros, y sus vidas a lo largo de los años.";
+let puntaje18 = 7.7;
+
+// LOGICA DEL PROGRAMA!!!
+
+let usuario = prompt('Ingrese su nombre:');
+
+alert('Bienvenido ' + usuario + ' a 1001 Peliculas que hay que ver antes de morir');
+
