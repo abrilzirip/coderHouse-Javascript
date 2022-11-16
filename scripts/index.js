b3e8b95f9d866b6bb0b09709bc6e2533a1d18f29
Fernando Piriz <fernandoapiriz@gmail.com>
2022-11-15 23:51:27 -0300

    Agregando para segunda entrega

--- scripts/index.js
+++ scripts/index.js
@@ -19,7 +19,7 @@ let titulo3 = "El gabinete del Dr. Caligari";
 let tituloOriginal3 = "Das Cabinet des Dr. Caligari";
 let anio3 = 1920;
 let duracion3 = 50; //Minutos
-let genero3 = "Teror, Misterio, Suspense";
+let genero3 = "Teror, Misterio, Suspenso";
 let sinopsis3 = "El doctor Caligari, un hipnotista, usa a Cesare, sonámbulo, para cometer asesinatos.";
 let puntaje3 = 8;
 
@@ -108,13 +108,13 @@ let titulo14 = "Pretty Woman";
 let tituloOriginal14 = "Pretty Woman";
 let anio14 = 1990;
 let duracion14 = 119; //Minutos
-let genero14 = "Coemdia, Romance";
+let genero14 = "Comedia, Romance";
 let sinopsis14 = "Un hombre en un negocio legal pero dañino necesita una acompañante para asistir a eventos sociales, y contrata a una hermosa prostituta... de la que se enamora.";
 let puntaje14 = 7.1;
 
 let titulo15 = "Jurassic Park (Parque Jurásico)";
 let tituloOriginal15 = "Jurassic Park";
-let anio15 = 1193;
+let anio15 = 1993;
 let duracion15 = 127; //Minutos
 let genero15 = "Acción, Aventura, Ciencia ficción";
 let sinopsis15 = "Un paleontólogo pragmático que recorre un parque temático casi completo en una isla de América Central tiene la tarea de proteger a un par de niños después de que un corte de energía hace que los dinosaurios clonados del parque se suelten.";
@@ -134,11 +134,151 @@ let anio17 = 2005;
 let duracion17 = 134; //Minutos
 let genero17 = "Drama, Romance";
 let sinopsis17 = "La historia de una relación prohibida y secreta entre dos vaqueros, y sus vidas a lo largo de los años.";
-let puntaje18 = 7.7;
+let puntaje17 = 7.7;
+
+generos = ['Acción', 'Animación', 'Aventura', 'Bélico', 'Ciencia ficción', 'Crimen', 'Comedia', 'Corto', 'Drama', 'Familiar', 'Hitoria', 'Misterio', 'Romance', 'Suspenso', 'Teror']
+
+let peliculas = [
+    {titulo: 'Viaje a la Luna', tituloOriginal: 'Le voyage dans la lune', año: 1902 , duracion: 13, genero: [generos[7], generos[0], generos[2]] , sinopsis: 'Un grupo de astrónomos realiza una expedición a la Luna.' , puntaje: 8.2},
+    {titulo: 'El nacimiento de una nación', tituloOriginal: 'The Birth of a Nation', año: 1915, duracion: 195, genero: [generos[3], generos[8], generos[10]] , sinopsis: 'La familia Stoneman encuentra su amistad con los Cameron afectados por la Guerra Civil, ambos luchando en ejércitos opuestos. El desarrollo de la guerra en sus vidas juega hasta el asesinato de Lincoln y el nacimiento del Ku Klux Klan.' , puntaje: 6.2},
+    {titulo: 'El gabinete del Dr. Caligari', tituloOriginal: 'Das Cabinet des Dr. Caligari', año: 1920, duracion: 50, genero: [generos[11], generos[14], generos[13]]  , sinopsis: 'El doctor Caligari, un hipnotista, usa a Cesare, sonámbulo, para cometer asesinatos.' , puntaje: 8},
+    {titulo: 'El moderno Sherlock Holmes', tituloOriginal: 'Sherlock Jr.', año: 1924, duracion: 45, genero: [generos[0], generos[6], generos[12]] , sinopsis: 'Un proyeccionista anhela convertirse en detective y la oportunidad se le presenta cuando un competidor le tiende una trampa.' , puntaje: 8.2},
+    {titulo: 'Tabú', tituloOriginal: 'Tabu: A Story of the South Seas', año: 1931, duracion: 86, genero: [generos[2], generos[8], generos[12]]  , sinopsis: 'En la isla de Bora Bora, en el Pacífico Sur, el amor de una joven pareja se ve amenazado cuando el jefe tribal declara a la niña virgen sagrada.' , puntaje: 7.4},
+    {titulo: 'Pinocho', tituloOriginal: 'Pinocchio', año: 1940, duracion: 86, genero: [generos[1], generos[2], generos[6]]  , sinopsis: 'Una marioneta cobra vida y, con un grillo como conciencia, debe demostrar que merece ser un niño de verdad.' , puntaje: 7.5},
+    {titulo: 'Casablanca', tituloOriginal: 'Casablanca', año: 1942, duracion: 102, genero: [generos[3], generos[8], generos[12]] , sinopsis: 'Un cínico expatriado norteamericano, dueño de un café, se debate entre ayudar o no a su antigua amante y a su marido fugitivo a escapar de los nazis en el Marruecos francés.' , puntaje: 8.5},
+    {titulo: 'La naranja mecánica', tituloOriginal: 'A Clockwork Orange', año: 1971, duracion: 136, genero: [generos[4], generos[5]] , sinopsis: 'En el futuro, el sádico líder de una banda es encarcelado y se ofrece como voluntario para un experimento de reeducación, pero no sale según lo planeado.' , puntaje: 8.3},
+    {titulo: 'El padrino', tituloOriginal: 'The Godfather', año: 1972, duracion: 175, genero: [generos[5], generos[8]] , sinopsis: 'El envejecido patriarca de una dinastía del crimen organizado en la ciudad de Nueva York de la posguerra transfiere el control de su imperio clandestino a su reacio hijo menor.' , puntaje: 9.2},
+    {titulo: 'Chinatown', tituloOriginal: 'Chinatown', año: 1974, duracion: 130, genero: [generos[8], generos[11], generos[13]] , sinopsis: 'Un detective privado, contratado para desenmascarar a un adúltero en Los Ángeles de los años 30, se ve envuelto en una red de engaños, corrupción y asesinato.' , puntaje: 8.2},
+    {titulo: 'Saló o los 120 días de Sodoma', tituloOriginal: 'Salò o le 120 giornate di Sodoma', año: 1975, duracion: 117, genero: [generos[8]] , sinopsis: 'Durante la Segunda Guerra Mundial en Italia, cuatro fascistas libertinos acorralan a nueve adolescentes y los someten a ciento veinte días de tortura física, mental y sexual.' , puntaje: 5.8},
+    {titulo: 'E.T. El extraterrestre', tituloOriginal: '', año: 182, duracion: 115, genero: [generos[2], generos[4], generos[9]] , sinopsis: 'Un niño con problemas se arma de valor para ayudar a un amistoso alienígena a escapar de la Tierra y regresar a su mundo natal.' , puntaje: 7.9},
+    {titulo: 'Regreso al futuro', tituloOriginal: 'Back to the Future', año: 1985, duracion: 116, genero: [generos[2], generos[6], generos[4]] , sinopsis: 'Marty McFly, un estudiante de secundaria de 17 años, es enviado accidentalmente 30 años al pasado en un DeLorean que viaja en el tiempo inventado por su amigo cercano, el científico inconformista Doc Brown.' , puntaje: 8.5},
+    {titulo: 'Pretty Woman', tituloOriginal: 'Pretty Woman', año: 1990, duracion: 119, genero: [generos[6], generos[12]] , sinopsis: 'Un hombre en un negocio legal pero dañino necesita una acompañante para asistir a eventos sociales, y contrata a una hermosa prostituta... de la que se enamora.' , puntaje: 7.1},
+    {titulo: 'Jurassic Park (Parque Jurásico)', tituloOriginal: 'Jurassic Park', año: 1993, duracion: 127, gener: [generos[0], generos[2], generos[4]] , sinopsis: 'Un paleontólogo pragmático que recorre un parque temático casi completo en una isla de América Central tiene la tarea de proteger a un par de niños después de que un corte de energía hace que los dinosaurios clonados del parque se suelten.' , puntaje: 8.2},
+    {titulo: 'Los padres de ella', tituloOriginal: 'Meet the Parents', año: 2000, duracion: 108, genero: [generos[6], generos[12]] , sinopsis: 'El enfermero Greg Focker conoce a los padres de su novia, a quien quiere pedirle que se case con él. El padre resulta ser la peor pesadilla de cualquier novio' , puntaje: 7},
+    {titulo: 'Secreto en la montaña', tituloOriginal: 'Brokeback Mountain', año: 2005, duracion: 108, genero: [generos[6], generos[12]] , sinopsis: 'El enfermero Greg Focker conoce a los padres de su novia, a quien quiere pedirle que se case con él. El padre resulta ser la peor pesadilla de cualquier novio' , puntaje: 7},
+]
 
 // LOGICA DEL PROGRAMA!!!
+let usuarios = [
+    {nombreUsuario: 'prueba', mail: 'prueba@prueba.com', contraseña: 'prueba'},
+    {nombreUsuario: 'fer', mail: 'fer@prueba.com', contraseña: 'fer'}
+];
 
-let usuario = prompt('Ingrese su nombre:');
 
-alert('Bienvenido ' + usuario + ' a 1001 Peliculas que hay que ver antes de morir');
+
+// console.log(usuarios[0].nombreUsuario)
+
+//  for (let variable of usuarios) {
+//      console.log(variable.nombreUsuario);
+//  }
+
+// nombre= "Pepe";
+// mail ="pepe@a.conm";
+// contraseña = "dassf5"
+
+// usuarios.push({nombreUsuario: 'abrilzirip', mail: 'apiriz@gmail.com', contraseña: '123456'})
+
+// usuarios.push({nombreUsuario: nombre, mail: mail, contraseña: contraseña})
+
+// for (let variable of usuarios) {
+//       console.log(variable);
+//     }
+
+
+
+
+// ** Menú principal **
+
+function
+
+// ** Menú Bienvenida **
+
+let op = prompt(`Bienvenidos a 1001 Peliculas que hay que ver antes de morir 
+ 1 - Iniciar sesion
+ 2 - Registarse
+ 3 - Olvido su contraña
+     Elija una opcion:`);
+
+do {
+    if (op == 1) {
+        let usuario = prompt('Ingrese su nombre de usuario');
+        let contra = prompt('Ingrese su contraseña');
+        // let user = false;
+        // let pass = false;
+
+    //     for (let variable of usuarios) {
+    //         if (variable.nombreUsuario === usuario) { 
+    //             if (variable.contraseña == contra) {
+    //                mensaje = alert('MENU PRINCIPAL');
+    //                break
+    //             }
+    //             else {
+    //                 mensaje = alert('El usuario y/o la contraseña es incorrecta.');
+    //                 break
+    //             }
+    //         }
+    //         else {
+    //             mensaje = alert('El usuario y/o la contraseña es incorrecta.');
+    //         }
+    //     }
+    //     mensaje;
+    //   }
+
+// mensaje = 'El usuario y/o la contraseña es incorrecta.';
+        // for (let variable of usuarios) {
+        //     if (variable.nombreUsuario === usuario) { 
+        //         user = true;
+        //         break;
+        //     }
+        //     else {
+        //             user = false;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // if (user !== false) {
+        //     for (let variable of usuarios) {
+        //         if (variable.contraseña === contra) { 
+        //             if (variable.contraseña == contra) {
+        //            user = true;
+        //            break;
+        //         }
+        //        else {
+        //             mensaje = 'El usuario y/o la contraseña es incorrecta.';
+        //             break;
+        //         }
+        //     }
+        //     }
+        // }
+
+        userValidar = usuarios.some(user => user.nombreUsuario === usuario);
+        passValidar = usuarios.some(user => user.contraseña === contra);
+
+        
+        if (userValidar && passValidar == true) {
+            alert('MENU PRINCIPAL')
+        }
+
+
+    }
+    else if (op == 2) {
+        let nombreUsuario = prompt('Ingrese su nombre de usuario:');
+        let mail = prompt('Ingrese su mail');
+        let contraseña = prompt('Ingrese su contraseña');
+        let contraseña2 = prompt('Confirmar su contraseña');
+    
+        while (contraseña !== contraseña2) {
+            alert('La contraseña ingresada no coincide. Por favor ingrese nuevamente');
+            contraseña = prompt('Ingrese su contraseña nuevamente');
+            contraseña2 = prompt('Confirmar su contraseña');
+        }
+        usuarios.push({nombreUsuario: nombreUsuario, mail: mail, contraseña: contraseña})
+
+        alert("Gracias por registarse en nuestra página web, ya puede iniciar secion");
+    }
+     else if (op == 3) {
+         alert('Elijio opnción 3');
+    }        
+ } while (op <= 1 && op >= 3);
 
